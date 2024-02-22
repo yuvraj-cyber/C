cc44408451f8fb521534e0efaee4e81be1577367
Yuvraj Singh <77912320+yuvraj-cyber@users.noreply.github.com>
2024-02-22 14:11:22 +0530

    Add files via upload

---
+++ Practice/3_Area_Parameter_Rectangle.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int l,b;
+
+    printf("Length of the Rectangle: ");
+    scanf("%d", &l);
+
+    printf("Breadth of the Rectangle: ");
+    scanf("%d", &b);
+
+    printf("Perimeter of the Rectangle is %d \n", 2*(l+b));
+
+    printf("Area of the Rectangle is %d", l*b);
+
+
+
+}
