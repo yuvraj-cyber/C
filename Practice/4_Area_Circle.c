4d384fb7d31f3ad68a5227cf42cd28534698296d
Yuvraj Singh <77912320+yuvraj-cyber@users.noreply.github.com>
2024-02-23 15:10:14 +0530

    Add files via upload

---
+++ Practice/4_Area_Circle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    float r;
+    float pi = 3.14;
+    printf("Radius = ");
+    scanf("%f", &r);
+
+    printf("Area of the Circle is %f", pi*r*r);
+
+    return 0;
+
+}
