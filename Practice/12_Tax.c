52329e0d17a56a01d0aaa901b21899dd698d4a2e
Yuvraj Singh <77912320+yuvraj-cyber@users.noreply.github.com>
2024-03-03 10:58:35 +0530

    Add files via upload

---
+++ Practice/12_Tax.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+int main()
+    {
+        float tax = 0, income;
+        printf("Enter your total income: ");
+        scanf("%f", &income);
+
+        /*if (income < 250000)
+            {
+                tax == 0;
+            }*/
+
+        if (income >= 250000 && income <500000)
+            {
+                tax = tax + 0.05*(income - 250000);
+            }
+        
+        if (income >= 500000 && income <= 100000)
+            {
+                tax += 0.20*(income - 500000);
+            }
+        
+        if (income > 100000)
+            {
+                tax += 0.30*(income - 100000);
+            }
+
+
+        printf ("Your tax is %f", tax);
+
+
+
+        return 0;
+    }
