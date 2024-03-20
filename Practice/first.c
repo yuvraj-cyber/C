334bb567038d7234ab2c0e79fac63886250e08b8
Yuvraj Singh <77912320+yuvraj-cyber@users.noreply.github.com>
2024-03-20 21:58:51 +0530

    Add files via upload

---
+++ Practice/first.c
@@ -0,0 +1,15 @@
+# include<stdio.h>
+int main()
+{
+    int a = 5;
+    float b = 8.5;
+    char  c = 'Avnfknvfd';
+    char  d = '&';
+    int e = 8;
+
+    printf("The value of a is %d \n", a);
+    printf("The value of b is %f \n", b);
+    printf("The value of c is %c \n", c);
+    printf("The value of d is %c \n", d);
+    printf("The value of a + b is %f \n", a + b);
+}
